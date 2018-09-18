fccacbe0c8541e38b35a9b0eda03ae98018569d4
Eiríkur Jóhannsson <eiki@live.com>
2018-09-18 09:16:15 +0000

    Add files via upload

---
+++ TileTraveller/TileTraveller1.py
@@ -0,0 +1,26 @@
+#Forritið keyrist á while loopu alveg upp að þeim tímapunkti þegar notandi vinnur leikinn á reit 3,1.
+#Við látum öll tile-in í sér breytur sem inniheldur sérhverja átt fyrir þann reit.
+
+tile11 = "N"
+tile12 = "NES"
+tile13 = "SE"
+tile21 = "N"
+tile22 = "SW"
+tile23 = "EW"
+tile32 = "NS"
+tile33 = "SW"
+possibilites=""
+player_tile_x=1
+player_tile_y=1
+victory = False
+while victory == False:
+    if player_tile_x == 1 and player_tile_y == 1: possibilites = tile11
+    if player_tile_x == 1 and player_tile_y == 2: possibilites = tile12
+    if player_tile_x == 1 and player_tile_y == 3: possibilites = tile13
+    if player_tile_x == 2 and player_tile_y == 1: possibilites = tile21
+    if player_tile_x == 2 and player_tile_y == 2: possibilites = tile22
+    if player_tile_x == 2 and player_tile_y == 3: possibilites = tile23
+    if player_tile_x == 3 and player_tile_y == 2: possibilites = tile32
+    if player_tile_x == 3 and player_tile_y == 3: possibilites = tile33
+
+    print ("You can travel: ", possibilites)
