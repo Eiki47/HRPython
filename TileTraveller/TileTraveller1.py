bc83cd42532133ee149671731ad689e7004b1c90
Eiríkur Jóhannsson <eiki@live.com>
2018-09-18 09:18:05 +0000

    Bætti við áttum

--- TileTraveller/TileTraveller1.py
+++ TileTraveller/TileTraveller1.py
@@ -1,3 +1,11 @@
 #Forritið keyrist á while loopu alveg upp að þeim tímapunkti þegar notandi vinnur leikinn á reit 3,1.
 #Við látum öll tile-in í sér breytur sem inniheldur sérhverja átt fyrir þann reit.
 
+tile11 = "N"
+tile12 = "NES"
+tile13 = "SE"
+tile21 = "N"
+tile22 = "SW"
+tile23 = "EW"
+tile32 = "NS"
+tile33 = "SW"
