af9afb5d1561a85cb6b3172ffa88615a1bd93a2f
Eiríkur Jóhannsson <eiki@live.com>
2018-09-18 09:21:23 +0000

    If setningar til að sjá möguleika í hreyfingu

--- TileTraveller/TileTraveller1.py
+++ TileTraveller/TileTraveller1.py
@@ -13,6 +13,15 @@ tile33 = "SW"
 player_tile_x=1
 player_tile_y=1
 
+possibilites = ""
+
 victory = False
-while victory == False:
-    
+while victory == False:   
+    if player_tile_x == 1 and player_tile_y == 1: possibilites = tile11
+    if player_tile_x == 1 and player_tile_y == 2: possibilites = tile12
+    if player_tile_x == 1 and player_tile_y == 3: possibilites = tile13
+    if player_tile_x == 2 and player_tile_y == 1: possibilites = tile21
+    if player_tile_x == 2 and player_tile_y == 2: possibilites = tile22
+    if player_tile_x == 2 and player_tile_y == 3: possibilites = tile23
+    if player_tile_x == 3 and player_tile_y == 2: possibilites = tile32
+    if player_tile_x == 3 and player_tile_y == 3: possibilites = tile33
