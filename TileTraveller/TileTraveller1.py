7202ba3457980a10fd3fd0e397fef32c37b44c3b
Eiríkur Jóhannsson <eiki@live.com>
2018-09-18 09:16:48 +0000

    Add files via upload

---
+++ TileTraveller/TileTraveller1.py
@@ -0,0 +1,3 @@
+#Forritið keyrist á while loopu alveg upp að þeim tímapunkti þegar notandi vinnur leikinn á reit 3,1.
+#Við látum öll tile-in í sér breytur sem inniheldur sérhverja átt fyrir þann reit.
+
